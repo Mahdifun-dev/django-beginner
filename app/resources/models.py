fda326a3f755897dcf3ce68129909ae75aedd020
Mahdi <mahdifun.microsoft@gmail.com>
2025-04-30 22:12:54 +0330

    create app and add models and register them

---
+++ app/resources/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    discription = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Resource(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    file = models.FileField(upload_to='resources/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    
+    
+class Comment(models.Model):
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.resource.title}'
