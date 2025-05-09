f2185453c311941a93894ad5f7ce0a83e32cf12c
Mahdi <mahdifun.microsoft@gmail.com>
2025-05-09 11:31:17 +0330

    fix typo

--- app/resources/models.py
+++ app/resources/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    discription = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
 
     def __str__(self):
         return self.name
