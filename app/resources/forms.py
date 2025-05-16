cef658baf1dc2d6a60f2f5dc8a31dc3431e36cd3
mahdifun-dev <mahdifun.microsoft@gmail.com>
2025-05-16 14:30:38 +0330

    repull

--- app/resources/forms.py
+++ app/resources/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 from .models import Resource
 
+
 class ResourceForm(forms.ModelForm):
     class Meta:
         model = Resource
-        fields = ['title', 'file', 'description']
-        
-        
+        fields = ["title", "file", "description", "category"]
+
+
 from .models import Comment
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['text']
+        fields = ["text"]
